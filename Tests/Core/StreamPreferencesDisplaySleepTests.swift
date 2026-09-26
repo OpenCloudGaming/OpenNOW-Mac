8581b8edd7c1ba99f187d8dafa6a599fc2fa7043
Anderson Shindy Oki <anderson.vs.oki@gmail.com>
2026-09-26 20:31:05 +0900

    test: serialise every writer of the shared preference domain

--- Tests/Core/StreamPreferencesDisplaySleepTests.swift
+++ Tests/Core/StreamPreferencesDisplaySleepTests.swift
@@ -2,22 +2,25 @@ import Testing
 @testable import OpenNOW
 
 @Test func displaySleepPreventionDefaultsOnAndPersists() {
-    let key = "OpenNOW.Stream.PreventDisplaySleepWhileStreaming"
-    let previous = OPNAppPreferenceStorage.standard.object(forKey: key)
-    defer {
-        if let previous {
-            OPNAppPreferenceStorage.standard.set(previous, forKey: key)
-        } else {
-            OPNAppPreferenceStorage.standard.removeObject(forKey: key)
+    withExclusivePreferenceDomain {
+        let key = "OpenNOW.Stream.PreventDisplaySleepWhileStreaming"
+        let previous = OPNAppPreferenceStorage.standard.object(forKey: key)
+        defer {
+            if previous == nil {
+                OPNAppPreferenceStorage.standard.removeObject(forKey: key)
+            }
+            if let previous {
+                OPNAppPreferenceStorage.standard.set(previous, forKey: key)
+            }
         }
+
+        OPNAppPreferenceStorage.standard.removeObject(forKey: key)
+        #expect(OPNStreamPreferences.loadProfile().preventDisplaySleepWhileStreaming)
+
+        OPNStreamPreferences.savePreventDisplaySleepWhileStreaming(false)
+        #expect(!OPNStreamPreferences.loadProfile().preventDisplaySleepWhileStreaming)
+
+        OPNStreamPreferences.savePreventDisplaySleepWhileStreaming(true)
+        #expect(OPNStreamPreferences.loadProfile().preventDisplaySleepWhileStreaming)
     }
-
-    OPNAppPreferenceStorage.standard.removeObject(forKey: key)
-    #expect(OPNStreamPreferences.loadProfile().preventDisplaySleepWhileStreaming)
-
-    OPNStreamPreferences.savePreventDisplaySleepWhileStreaming(false)
-    #expect(!OPNStreamPreferences.loadProfile().preventDisplaySleepWhileStreaming)
-
-    OPNStreamPreferences.savePreventDisplaySleepWhileStreaming(true)
-    #expect(OPNStreamPreferences.loadProfile().preventDisplaySleepWhileStreaming)
 }
--- Tests/Core/StreamPreferencesReflexTests.swift
+++ Tests/Core/StreamPreferencesReflexTests.swift
@@ -2,22 +2,25 @@ import Testing
 @testable import OpenNOW
 
 @Test func reflexDefaultsOnAndPersists() {
-    let key = "OpenNOW.Stream.ReflexEnabled"
-    let previous = OPNAppPreferenceStorage.standard.object(forKey: key)
-    defer {
-        if let previous {
-            OPNAppPreferenceStorage.standard.set(previous, forKey: key)
-        } else {
-            OPNAppPreferenceStorage.standard.removeObject(forKey: key)
+    withExclusivePreferenceDomain {
+        let key = "OpenNOW.Stream.ReflexEnabled"
+        let previous = OPNAppPreferenceStorage.standard.object(forKey: key)
+        defer {
+            if previous == nil {
+                OPNAppPreferenceStorage.standard.removeObject(forKey: key)
+            }
+            if let previous {
+                OPNAppPreferenceStorage.standard.set(previous, forKey: key)
+            }
         }
+
+        OPNAppPreferenceStorage.standard.removeObject(forKey: key)
+        #expect(OPNStreamPreferences.loadProfile().enableReflex)
+
+        OPNStreamPreferences.saveReflexEnabled(false)
+        #expect(!OPNStreamPreferences.loadProfile().enableReflex)
+
+        OPNStreamPreferences.saveReflexEnabled(true)
+        #expect(OPNStreamPreferences.loadProfile().enableReflex)
     }
-
-    OPNAppPreferenceStorage.standard.removeObject(forKey: key)
-    #expect(OPNStreamPreferences.loadProfile().enableReflex)
-
-    OPNStreamPreferences.saveReflexEnabled(false)
-    #expect(!OPNStreamPreferences.loadProfile().enableReflex)
-
-    OPNStreamPreferences.saveReflexEnabled(true)
-    #expect(OPNStreamPreferences.loadProfile().enableReflex)
 }
